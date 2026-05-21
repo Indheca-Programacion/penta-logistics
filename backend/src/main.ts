244bb8b0ad372acfa8436e615af4f2af6ee30f14
Brian Salas <salas.pardo13@gmail.com>
2026-05-21 10:29:15 -0600

    feat: habilite cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,6 +11,11 @@ async function bootstrap() {
       transform: true, // Transforma los tipos automáticamente (ej. string a number)
     }),
   );
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.API_PORT ?? 3000);
 }
 bootstrap();
