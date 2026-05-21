2604aa19606365144a6ced22a7f49e52ae84fac9
Brian Salas <salas.pardo13@gmail.com>
2026-05-21 10:55:42 -0600

    add: añadi ruta de api

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '@/store/authStore';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1', // Asegúrate de que el backend tenga el mismo prefijo global
 });
 
 // Interceptor de salida: Inyecta el token dinámicamente
