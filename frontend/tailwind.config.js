b56d8f82314dc691564ef8b4256c2217d4bb25e4
Brian Salas <salas.pardo13@gmail.com>
2026-05-22 11:13:08 -0600

    add: añadi colores

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+          brand: {
+            primary: '#0F4B65', // Example primary color
+            secondary: '#9FBD41', // Example secondary color
+            background: '#F5F5F5', // Example background color
+            surface: '#FFFFFF', // Example surface color
+            text: '#333333', // Example text color
+            accent: '#f07e74', // Example accent color
+          },
+        },
+    },
   },
   plugins: [],
 }
